03491fcf5a88945e6386598ad1271c5dabf43589
Armando Toaquiza <luis.toaquiza5731@utc.edu.ec>
2026-02-18 19:32:54 -0500

    Arreglar conexión a IP

--- Sistema/Sistema/settings.py
+++ Sistema/Sistema/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-17(0qy#of_#!v%6!2r_i+b$zk9p^ipt+d%c72e5xb*gn$!0k@q
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.1.18', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.144.184.155', 'localhost', '127.0.0.1']
 
 
 # Application definition
